13c29ce583dab97ba1f0fa0a006547517cd828e3
Chikootlin <dhyckoritchie@gmail.com>
2025-09-03 12:44:22 +0700

    feat:implemen-login-page

--- routes/web.php
+++ routes/web.php
@@ -7,5 +7,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/login', function () {
+    return view('login');
+});
+
+
 Route::get('/students', [StudentController::class, 'index']);
 Route::get('/students/{id}', [StudentController::class, 'show'])->name('students.show');
